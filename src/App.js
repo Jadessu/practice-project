b2a6d6153854853e8ade08c03d45de0ef139ffe2
jadessu <junioradessu@gmail.com>
2021-11-03 21:32:24 -0500

    add css to banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Banner from './Banner';
 function App() {
   return (
     <div className="App">
-     
+     <Banner/>
     </div>
   );
 }
--- src/Banner.jsx
+++ src/Banner.jsx
@@ -6,9 +6,15 @@ function Banner() {
         <div className="wrapper">
             <section className="banner">
                 <div className="banner-content">
+                    <div className="title">
+
                     <h1>Welcome To Highline</h1>
+                    </div>
                     <div className="banner-info">
-                        <p>Whether you're looking for a euphoric or a relaxing experience, Highline delivers local New York cannabis right to your doorstep.</p>
+                        <p>Whether you're looking for a euphoric or a relaxing<br/> experience, Highline delivers local New York cannabis right to<br/> your doorstep.</p>
+                        <div className="button">
+                            <button>Get High</button>
+                        </div>
                     </div>
                 </div>
             </section>
