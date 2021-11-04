55f64cda0ab923b859b6daf38e173c2dc291bcaa
jadessu <junioradessu@gmail.com>
2021-11-03 21:18:33 -0500

    add banner info

---
+++ src/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+    return (
+        <div className="wrapper">
+            <section className="banner">
+                <div className="banner-content">
+                    <h1>Welcome To Highline</h1>
+                    <div className="banner-info">
+                        <p>Whether you're looking for a euphoric or a relaxing experience, Highline delivers local New York cannabis right to your doorstep.</p>
+                    </div>
+                </div>
+            </section>
+            
+        </div>
+    )
+}
+
+export default Banner
